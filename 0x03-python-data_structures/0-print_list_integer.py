7429021d0eb70ce85a58ab329168731999dd1ca5
Nathanaelfowler <natafowler1@gmail.com>
2022-12-03 08:37:18 -0800

    Task 0: renamed file

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,13 +2,13 @@
 
 def print_list_integer(my_list=[]):
 
-    my_list.append(1)
+    '''my_list.append(1)
     my_list.append(2)
     my_list.append(3)
     my_list.append(4)
-    my_list.append(5)
+    my_list.append(5)'''
 
-    for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+    for i in my_list:
+        print("{:d}".format(i))
 
-print_list_integer()
+#print_list_integer()
