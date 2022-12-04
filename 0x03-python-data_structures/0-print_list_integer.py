4ff7c4292b51c07f8183997af9971373e0c88b7b
Nathanaelfowler <natafowler1@gmail.com>
2022-12-04 15:38:22 -0800

    Task 0: printing a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-
-    '''my_list.append(1)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(4)
-    my_list.append(5)'''
-
-    for i in my_list:
-        print("{:d}".format(i))
-
-#print_list_integer()
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
