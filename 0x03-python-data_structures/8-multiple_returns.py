46133797a1a234df224361635b5290329e83ddbe
Nathanaelfowler <natafowler1@gmail.com>
2022-12-05 14:00:14 -0800

    Task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        sentence[idx] = None
+    for idx in sentence:
+        length = len(sentence)
+        first_char = sentence[idx]
+    return length
+    return first_char
