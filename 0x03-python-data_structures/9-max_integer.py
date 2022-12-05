715665c8f3ef5494f6e2cc5a8a35f029bd496314
Nathanaelfowler <natafowler1@gmail.com>
2022-12-05 07:41:34 -0800

    Task 9: finding the max num

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    else:
-        biggest = 0
-        for i in len(my_list):
-            if i > biggest:
-                biggest = i
-            return biggest
+    biggest = 0
+    for i in my_list:
+        if i > biggest:
+            biggest = i
+    return biggest
