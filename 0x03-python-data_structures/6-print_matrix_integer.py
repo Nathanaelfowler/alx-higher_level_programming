c2046de3d5547f98d2886a8f3d66d6b6e5427551
Nathanaelfowler <natafowler1@gmail.com>
2022-12-05 02:13:48 -0800

    Task 6: printing a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(Row):
         for column in range(Column):
-            print("{:d}".format(matrix[row][column]))
+            print("{:d} {:d}".format(row, column))
