094e01ce73961d5510b7d526296284b16614308e
Nathanaelfowler <natafowler1@gmail.com>
2022-12-05 13:17:04 -0800

    Task 10: returning a list of multiples of two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list
+    if not my_list:
+        return my_list
+    new_list = list(my_list)
     for idx in len(my_list):
-        if my_list[idx] % 2 == 0:
+        if new_list[idx] % 2 == 0:
             return True
-        if my_list[idx] % 2 != 0:
+        if new_list[idx] % 2 != 0:
             return False
     return new_list
