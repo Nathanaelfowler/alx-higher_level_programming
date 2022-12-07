89dbff0d08f8d01dfb6f7f936557ce4134fdf505
Nathanaelfowler <natafowler1@gmail.com>
2022-12-07 06:09:26 -0800

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for replace, search in enumerate(my_list):
-        if search == x:
-            my_list[replace] = y
+    new_list = [x if x != search else replace for x in my_list]
     return new_list
