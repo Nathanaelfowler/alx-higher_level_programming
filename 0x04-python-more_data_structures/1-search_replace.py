0dfdb0435f23d216063f3abfeb6958eb286391f7
Nathanaelfowler <natafowler1@gmail.com>
2022-12-07 06:00:16 -0800

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,6 @@
-
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for replace, search in enumerate(my_list):
+        if search:
+            my_list[replace] += 1
+    return my_list
