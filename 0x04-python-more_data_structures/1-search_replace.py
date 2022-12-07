c3fcb6a4e1fb73597bd3dc3675157384f07805a0
Nathanaelfowler <natafowler1@gmail.com>
2022-12-07 06:01:37 -0800

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 def search_replace(my_list, search, replace):
     for replace, search in enumerate(my_list):
         if search:
-            my_list[replace] += 1
+            my_list[replace]
     return my_list
