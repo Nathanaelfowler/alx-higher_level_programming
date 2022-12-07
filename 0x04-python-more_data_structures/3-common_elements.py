1fc3a7c3052f01a308ecf62c1e98567c29abc61d
Nathanaelfowler <natafowler1@gmail.com>
2022-12-07 06:52:43 -0800

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     set_3 = set()
-    for s1 in set_1:
-        for s2 in set_2:
-            set_3 = s1 & s2
+    set_3 = s1 & s2
     return set_3
