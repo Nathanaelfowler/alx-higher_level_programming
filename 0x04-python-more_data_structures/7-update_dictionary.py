4c1dd202b2bd7189560bb017316e997fc02a87df
Nathanaelfowler <natafowler1@gmail.com>
2022-12-07 14:06:34 -0800

    Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,7 @@
-
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        print()
+    print("{}: {}".format(key, value))
