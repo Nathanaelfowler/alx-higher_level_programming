2bc8df125cdde55df86ab9ead92790fb5e746ae0
Nathanaelfowler <natafowler1@gmail.com>
2022-12-07 23:14:41 -0800

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,9 +3,12 @@ def square_matrix_simple(matrix=[]):
     if not matrix:
         print()
     new_matrix = []
-    new_matrix = [[1]] * len(matrix)  #creating new matrix as the same size of matrix
-    row_index = 0  #indexing no. of row
+    new_matrix = [[1]] * len(matrix)
+    #creating new matrix as the same size of matrix
+    row_index = 0
+    #indexing no. of row
     for row in matrix:
-        new_matrix[row_index] = [x**2 for x in row]  #x represents the numbers on a particular row index
+        new_matrix[row_index] = [x**2 for x in row]
+        #x represents the numbers on a particular row index
         row_index += 1
     return new_matrix
