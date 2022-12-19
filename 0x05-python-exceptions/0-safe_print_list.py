1a067116774793a6d1aeb6776a42b0cf90a0a397
Nathanaelfowler <natafowler1@gmail.com>
2022-12-19 08:54:32 -0800

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        length = sum([1 for i in my_list])
-        for x in range(length):
-            if x < length:
-                print("{}".format(my_list[type(x)]))
+        count = 0
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
     except Exception:
       print('Something went wrong')
+
+    print()
+    return count
