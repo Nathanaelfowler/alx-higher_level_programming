f80e66a18ed3121fd92cb6b4b79ec4fc2c117f48
Nathanaelfowler <natafowler1@gmail.com>
2022-12-19 10:33:58 -0800

    Task 5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
     try:
-        raise Exception
-    except Exception as e:
+        raise TypeError
+    except TypeError as te:
         print('Error')
