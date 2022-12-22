1b3c1d60793b03dd3fd86185e20f7bb43efa6497
Nathanaelfowler <natafowler1@gmail.com>
2022-12-21 23:30:04 -0800

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-class Square():
+"""compulsory line"""
+class Square:
     """defining a square"""
     def __init__(self, size):
-        """initializing a method"""
+        """initializing a method
+        Args:
+            size is an int which is the size of the square"""
         self.__size = size
