9dba147cce219579fde7d50216f7f10878235c39
Nathanaelfowler <natafowler1@gmail.com>
2022-12-22 00:51:19 -0800

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""compulsory line"""
+
+
+class Square:
+    """defining a class"""
+
+    def __init__(self, size=0):
+        """using a method"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+
+    def area(self):
+        """a method for the area"""
+        return int(self.__size)
