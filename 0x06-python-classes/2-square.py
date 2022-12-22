a75ed977663e8355f0036ee41f12aa8fc9d92317
Nathanaelfowler <natafowler1@gmail.com>
2022-12-22 00:36:52 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,6 +9,6 @@ class Square:
         """using a method"""
         self.__size = size
         if type(size) != int:
-                raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise TypeError("size must be >= 0")
