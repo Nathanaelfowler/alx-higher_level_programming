a805cec335ed73ed47b4b38639e41d1dd2ea48fd
Nathanaelfowler <natafowler1@gmail.com>
2023-01-04 11:45:51 -0800

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
             """setter"""
             if type(value) != int:
                 raise TypeError("width must be an integer")
-            elif width < 0:
+            elif value < 0:
                 raise ValueError("width must be >= 0")
             else:
                 self.__width = value
@@ -41,7 +41,7 @@ class Rectangle:
             """setter"""
             if type(value) != int:
                 raise TypeError("height must be an integer")
-            elif height < 0:
+            elif value < 0:
                 raise ValueError("height must be >= 0")
             else:
                 self.__height = value
