9ed37157280a263d6c89ae9bfa34a7325a13675c
Nathanaelfowler <natafowler1@gmail.com>
2023-01-04 07:08:29 -0800

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""compulsory line"""
+
+
+class Rectangle:
+    """defining a class"""
+
+    def __init__(self, width=0, height=0):
+        """using the 'init' method"""
+        self.__width = width
+        self.__height = height
+
+        #For width:
+        #getter
+        @property
+        def width(self):
+            """defining the width"""
+            return self.__width
+
+        #setter
+        @width.setter
+        def width(self, value):
+            """setter"""
+            if type(width) != int:
+                raise TypeError("width must be an integer")
+            elif width < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        #For height:
+        #getter
+        @property
+        def height(self):
+            """defining the height"""
+            return self.__height
+
+        #setter
+        @height.setter
+        def height(self, value):
+            """setter"""
+            if type(height) != int:
+                raise TypeError("height must be an integer")
+            elif height < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+
