0d5fd07e51a8da0a28a5ffa427d8a3d794850e49
Nathanaelfowler <natafowler1@gmail.com>
2023-01-09 12:43:06 -0800

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a compulsory line"""
+
+
+def lookup(obj):
+    """a function that returns the list of available attributes and methods of an object"""
+
+    def __init__(self, list):
+        """using a constructor"""
+        self.list = list
+l = lookup(list)
