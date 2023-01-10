a8e77483c4963f41a5a748e2dfb4a8f02b4dc748
Nathanaelfowler <natafowler1@gmail.com>
2023-01-10 12:53:38 -0800

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a compulsory line"""
+
+
+class BaseGeometry:
+    """a class"""
+
+    def area(self):
+        """a method"""
+        raise Exception("area() is not implemented")
