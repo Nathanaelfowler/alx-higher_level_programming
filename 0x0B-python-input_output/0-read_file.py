7192eac4ca5614a5f692b8d377b94c1f24845c7a
Nathanaelfowler <natafowler1@gmail.com>
2023-01-16 12:26:27 -0800

    Task 0 and a README.md file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a compulsory line"""
+
+
+def read_file(filename=""):
+    """a func. to read a file"""
+    with open('""', encoding='utf-8') as a_file:
+       afile = a_file()
+       print(afile)
