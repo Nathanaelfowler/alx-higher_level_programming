e07a57702d061557d69a3fc4a4140e325ca5e25d
Nathanaelfowler <natafowler1@gmail.com>
2023-01-16 12:45:44 -0800

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """a func. to read a file"""
-    with open('filename=""', encoding="UTF-8") as a_file:
-       afile = a_file()
+    with open(filename, "r", encoding="UTF-8") as a_file:
+       afile = a_file.read()
        print(afile, end="")
