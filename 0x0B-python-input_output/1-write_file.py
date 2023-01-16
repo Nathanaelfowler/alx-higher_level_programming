292ffa43431cef48f2063528970839da0c70b736
Nathanaelfowler <natafowler1@gmail.com>
2023-01-16 12:53:35 -0800

    Task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a compulsory line"""
+
+
+def write_file(filename="", text=""):
+    """a func. that writes to a file"""
+    with open(filename, "w", encoding="UTF-8") as a_file:
+        afile = a_file.write(text)
+        print(afile, end="")
