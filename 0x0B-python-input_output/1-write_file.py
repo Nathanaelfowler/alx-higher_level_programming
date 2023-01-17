b7c97e4c66ac184382ca4a74e0ab53f41c82b152
Nathanaelfowler <natafowler1@gmail.com>
2023-01-17 12:44:23 -0800

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,6 @@
 
 def write_file(filename="", text=""):
     """a func. that writes to a file"""
-    with open(filename, "w", encoding="UTF-8") as a_file:
+    with open(filename, "w") as a_file:
         afile = a_file.write(text)
-        print(afile, end="")
-        count = a_file.tell()
-        return count
+        return afile
