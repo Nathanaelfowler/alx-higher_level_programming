af54c400522507d4d5cd7374cc05e3bf047dc886
Nathanaelfowler <natafowler1@gmail.com>
2023-01-18 12:25:56 -0800

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,8 +2,12 @@
 """a compulsory line"""
 
 
+import json
+
+
 def save_to_json_file(my_obj, filename):
     """a func. to write an Object to a text file,
     using a JSON representation"""
+    myobj = json.dumps(my_obj)
     with open(filename, "w") as a_file:
-        afile = a_file.write(my_obj)
+        afile = a_file.write(myobj)
