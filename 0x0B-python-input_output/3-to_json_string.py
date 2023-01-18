a69838735f4ddbf4b18a6780a09a3e91b522fce4
Nathanaelfowler <natafowler1@gmail.com>
2023-01-18 11:58:20 -0800

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,6 +3,8 @@
 
 
 import json
+
+
 def to_json_string(my_obj):
     """a func. to return the JSON representation of an object (string)"""
     myobj = json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a compulsory line"""
+
+
+import json
+
