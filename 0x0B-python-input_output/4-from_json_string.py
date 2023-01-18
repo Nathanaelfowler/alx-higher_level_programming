32b2678de737937194d528288fdea90f86de2ae8
Nathanaelfowler <natafowler1@gmail.com>
2023-01-18 12:01:33 -0800

    Task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,3 +4,8 @@
 
 import json
 
+
+def from_json_string(my_str):
+    """a func. that returns an object (Python data structure) represented by a JSON string"""
+    mystr = json.loads(my_str)
+    return mystr
