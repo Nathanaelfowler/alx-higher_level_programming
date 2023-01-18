c1d6ed5c34901cc986b9f41e6de778335c2e23b4
Nathanaelfowler <natafowler1@gmail.com>
2023-01-18 12:51:58 -0800

    Task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,4 +9,4 @@ def load_from_json_file(filename):
     """a func. to create an Object from a “JSON file”"""
     with open(filename, "r", encoding="UTF-8") as a_file:
         afile = json.loads(a_file.read())
-        """return afile"""
+        return afile
